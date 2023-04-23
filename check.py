6ba896aa4dcd797bbc9e79c1d1a6f5464e4d428a
3ngin33r0d1y <67762227+3ngin33r0d1y@users.noreply.github.com>
2023-04-23 03:50:48 +0200

    Update check.py

--- check.py
+++ check.py
@@ -1,14 +1,8 @@
 from datetime import datetime
-
-import requests
-import smtplib
 from email.mime.text import MIMEText
 from bs4 import BeautifulSoup
 import time
-
-
-
-
+import subprocess
 
 # Phrase to look for on the websites
 target_phrase = "Out of Stock"
@@ -20,50 +14,50 @@ smtp_server = "smtp-pulse.com"
 smtp_port = 587
 smtp_password = "oTHcGJbJ5rdQPj"
 
+
 # Loop indefinitely
 while True:
-    # Scrape each website in the list
-    urls = ['https://www.calvetsupply.com/capsules-and-tablets-fish-aid-antibiotics.html', 'https://www.calvetsupply.com/Capsules-Powders-and-Tablets_c_330-2.html'] # Replace with the URL of the webpage you want to scrape
+    # List of websites to scrape
+    urls = ['https://www.calvetsupply.com/capsules-and-tablets-fish-aid-antibiotics.html',
+            'https://www.calvetsupply.com/Capsules-Powders-and-Tablets_c_330-2.html'] # Replace with the URL of the webpage you want to scrape
 
     for url in urls:
-        response = requests.get(url)
+        # Make GET request using curl
+        command = ['curl', '-s', url]
+        response = subprocess.check_output(command, universal_newlines=True)
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+        # Parse response as HTML
+        soup = BeautifulSoup(response, 'html.parser')
 
         names = soup.find_all(class_='name')
-        # List of websites to scrape
-        websites = []
 
         for name in names:
             link = name.find('a')
             if link:
-                #print("https://www.calvetsupply.com/" + link['href'])
+                # Make GET request to individual product page using curl
+                product_url = "https://www.calvetsupply.com/" + link['href']
+                command = ['curl', '-s', product_url]
+                product_response = subprocess.check_output(command, universal_newlines=True)
 
-                response = requests.get("https://www.calvetsupply.com/" + link['href'])
-                soup = BeautifulSoup(response.content, "html.parser")
-                content = soup.get_text()
-                title = soup.title.string
+                product_soup = BeautifulSoup(product_response, "html.parser")
+                content = product_soup.get_text()
+                title = product_soup.title.string
 
                 # Check if the target phrase is in the website content or title
                 if target_phrase in content or target_phrase in title:
                     # Send email alert
-                    message = f"Subject: {link['href']} IS OUT OF STOCK"
+                    message = f"Subject: {link['href']} is out of stock"
                     msg = MIMEText(message)
                     msg['From'] = email
                     msg['To'] = to_email
-                    #print('https://www.calvetsupply.com/' + link + " \t is \t" + target_phrase)
-                    print(f" {link['href'] } is out of stock ")
-                    with smtplib.SMTP(smtp_server, smtp_port) as server:
-                        server.starttls()
-                        server.login(to_email, smtp_password)
-                        server.sendmail(email, to_email, msg.as_string())
+                    print(f"{product_url} is out of stock")
                 else:
-                     print(f"{title} in stock")
-                   
+                    print()
+                    print(f"{title} is available")
+                    print()
 
     # Wait for 10 minutes before checking again
-    time.sleep(3600)
+    time.sleep(500)
     print("===========================================================")
     print(datetime)
     print("===========================================================")
-
